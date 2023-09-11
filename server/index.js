e9909bdccb8b730869e6d07a6c64526ea01a15a5
Suchith Kumar G M <suchithkumargm@gmail.com>
2023-09-11 18:04:04 +0530

    starting files

---
+++ server/index.js
@@ -0,0 +1,7 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+console.log('server started at http://localhost:5000');
