3dd54fb5cc8517ea824acc8d366e157442015699
Suchith Kumar G M <suchithkumargm@gmail.com>
2023-09-12 15:35:20 +0530

    safety commit for rollback after major changes

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import cors from 'cors';
 import connectToMongo from './db.js';
 import userAuthRoute from './routes/auth/userAuth.js';
 import managerAuthRoute from './routes/auth/managerAuth.js';
+import trainComplaintsRoute from './routes/complaints/trainComplaint.js';
+import stationComplaintsRoute from './routes/complaints/stationComplaint.js';
 
 connectToMongo();   //database connection
 const app = express();  //create an express app
@@ -14,6 +16,8 @@ app.use(express.json());
 
 app.use('/auth/user', userAuthRoute);     //Route for user authentication
 app.use('/auth/manager', managerAuthRoute);     //Route for manager authentication
+app.use('/complaints/traincomplaint', trainComplaintsRoute);     //Route for manager authentication
+app.use('/complaints/stationcomplaint', stationComplaintsRoute);     //Route for manager authentication
 
 app.listen(port, () => {
     console.log(`server started on http://localhost:${port}`);
---
+++ server/routes/complaints/trainComplaint.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import { body, validationResult } from 'express-validator';
+
+import fetchuser from '../middleware/fetchUser.js';
+import TrainComplaint from '../models/TrainComplaint.js';
+
+const router = express.Router();
+
+// // ROUTE 1: Get All the Notes using: GET "/api/notes/fetchallnotes". Login required
+// router.get('/fetchallnotes', fetchuser, async (req, res) => {
+//     const notes = await Note.find({ user: req.user.id });
+//     res.json(notes)
+// })
+
+// ROUTE 2: lodge a new complaint using: POST "/complaints/add/traincomplaint". user Login required
+router.post('/add/traincomplaint', fetchuser, [
+    body('title', 'Enter a valid name').isLength({ min: 3 }),
+    body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),
+], async (req, res) => {
+    try {
+
+        const { title, description, tag } = req.body;
+        // If there are errors, return bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const note = new Note({
+            title, description, tag, user: req.user.id
+        })
+        const savedNote = await note.save()
+        res.json(savedNote)
+
+    } catch (error) {
+        console.error();
+        res.status(500).send("Internal Server Error");
+    }
+})
